e892439e0c2d9e1beaa0c9ed6ece27830800bf8f
Ross Simpson <ross.simpson@gmail.com>
2018-03-21 11:27:33 +0200

    Modified proxy bar for Bibblio RCM settings

--- themes/default/framedForm.inc.php
+++ themes/default/framedForm.inc.php
@@ -1,7 +1,7 @@
 <style type="text/css">
    /* Make room for the mini-form */
    html body { margin-top: 60px; }
-   
+
    /* Reset all styles */
    #include * {
       text-align: left;
@@ -23,19 +23,19 @@
    #include script {
       display:none;
    }
-   
+
    /* Style the mini-form div */
    #include {
       border-top: 3px solid #ce6c1c;
       border-bottom: 3px solid #ce6c1c;
       background: #0b1933;
-      position: absolute;
+      position: fixed;
       top:0; left:0;
       width: 100%;
-      height: 50px;
+      height: 60px;
       z-index: 100000;
    }
-   
+
    /* Mini-form elements */
    #include a {
       color: #ce6c1c;
@@ -61,6 +61,55 @@
    }
 </style>
 <div id="include">
+
+<link rel="stylesheet" type="text/css" href="/bibblio-related-content-module/css/bib-related-content.css">
+<script src="/bibblio-related-content-module/js/bib-related-content.js"></script>
+
+
+<script>
+   function insertRCM() {
+      var selector = document.getElementById('rcm-elem').value;
+
+      // class name
+      if (selector[0] == '.') {
+         var selector = selector.substring(1);
+         var parent = document.getElementsByClassName(selector)[0];
+
+      // id
+      } else if (selector[0] == '#') {
+         var selector = selector.substring(1);
+         var parent = document.getElementById(selector);
+
+      // default to id
+      } else {
+         var parent = document.getElementById(selector);
+      }
+
+      var rcm = document.createElement("div");
+      rcm.setAttribute("id", "bib_related-content");
+
+      parent.appendChild(rcm);
+      showRCM();
+   }
+
+   function showRCM() {
+     Bibblio.initRelatedContent(
+       {
+           styleClasses: "bib--title-only bib--shine bib--col-3 bib--square", // Custom CSS classes
+           showRelatedBy: true,
+           subtitleField: 'provider.name',  // default: headline. passing a value of false will disable the subtitle completely
+           contentItemId: '1cc62426-cc6a-4c28-8dd1-a3e566790d30',
+           recommendationKey: 'da2747ea-c10d-4407-a90d-69e6121612ff',
+           targetElementId: 'bib_related-content'
+       },
+       {
+         onRecommendationClick: function(activityData, event) {
+           // Do callback implementation
+         }
+       });
+   }
+</script>
+
 <?php
 // Print form using variables (saves repeatedly opening/closing PHP tags)
 // Edit as if normal HTML but escape any dollar signs
@@ -68,31 +117,26 @@ echo <<<OUT
    <form action="{$proxy}/includes/process.php?action=update" target="_top" method="post" onsubmit="return updateLocation(this);">
 
       <p>
-         
+
          <b>URL:</b>
          <input type="text" name="u" size="40" value="{$url}" class="url-input" style="width:50%;" />
          <input type="submit" value="Go" class="url-input url-button" />
-         
+
          [<a href="{$proxy}/index.php" target="_top">home</a>]
          [<a href="{$proxy}/includes/process.php?action=clear-cookies&return={$return}" target="_top">clear cookies</a>]
-         
+
       </p>
-      
+
       <p>
-         <b>Options:</b>
+
+         <b>DOM element to append to:</b>
+         <input type="text" name="rcm-elem" id="rcm-elem" size="40" value="left-hand-column" class="url-input" style="width:25%;" />
+         <input type="button" value="Insert RCM" class="url-input url-button" onClick="javascript:insertRCM()" />
 OUT;
 
-// Loop through the options and print with appropriate checkedness
-foreach($toShow as $details) { 
-   echo <<<OUT
-         <input type="checkbox" name="{$details['name']}" id="{$details['name']}"{$details['checked']} />
-         <label for="{$details['name']}">{$details['title']}</label>
-
-OUT;
-}
 ?>
       </p>
-      
+
    </form>
 
 </div>
